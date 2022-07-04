e3da66386e2ab028520488f248eff401ecd92536
Nasim Bin Kamal <nasimbinkamal@gmail.com>
2022-07-04 23:51:00 +0600

    fetch and post request implemented

---
+++ src/components/Classes.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+
+const Classes = () => {
+    const [classes, setClasses] = useState([]);
+    useEffect(() => {
+        fetch('https://62c30133876c4700f5345ece.mockapi.io/classes')
+            .then(res => res.json())
+            .then(data => setClasses(data));
+    }, []);
+
+
+    const handleSubscribe = (id) => {
+
+        const data = { classId: id, subscribe: true };
+        axios.post('https://62c30133876c4700f5345ece.mockapi.io/subscribe', data)
+            .then(res => {
+                console.log(res);
+                if (res?.data?.subscribe === true) {
+                    toast.success('Subscribed Successfully', {
+                        autoClose: 1000,
+                        position: 'top-center'
+                    });
+
+                }
+
+            });
+    }
+
+    return (
+        <div >
+            <ToastContainer />
+            <div className='container mx-2 '>
+
+                <h1 className='sm:text-lg lg:text-3xl text-center my-5'>See All Classes</h1>
+                <div className='mx-2 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+                    {
+                        classes?.map((singleClass => (<div key={singleClass.id}
+                            className="p-3 rounded-lg bg-blue-100"
+                        >
+                            <h3 className='sm:text-lg lg:text-xl text-black'>Teacher Name: {singleClass?.name}</h3>
+                            <h5 className='text-lg'>Teacher Names On {singleClass?.time}</h5>
+
+                            <button
+                                id='btn-subscribe'
+                                onClick={() => handleSubscribe(singleClass.id)}
+                                className="bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-3 rounded-full">
+
+                                Subscribe
+                            </button>
+
+                        </div>)))
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Classes;
